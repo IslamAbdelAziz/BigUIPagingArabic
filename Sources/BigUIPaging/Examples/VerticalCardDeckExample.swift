4d1d844a7ae67b98a51f26eb58607f989e2e0eac
iSlam Abdelaziz <islam.abdelaziz.d@gmail.com>
2025-12-09 10:09:02 +0200

    [+] feat: create vertical card deck

---
+++ Sources/BigUIPaging/Examples/VerticalCardDeckExample.swift
@@ -0,0 +1,67 @@
+//  VerticalCardDeckExample.swift
+//  BigUIPagingArabic
+//
+//  Created by iSlam AbdelAziz on 07/12/2025.
+//
+
+import SwiftUI
+
+/// An example of how to create a deck of cards in a vertical stack.
+@available(macOS, unavailable)
+struct VerticalCardDeckExample: View {
+    @Environment(\.layoutDirection) var layoutDirection
+
+    @State private var selection: Int = 1
+    private let totalPages = 10
+    
+    var body: some View {
+        VStack {
+            PageeView(selection: $selection, direction: layoutDirection) {
+                ForEach(1...totalPages, id: \.self) { value in
+                    ExamplePage(value: value)
+                        // Resize to be more card-like.
+                        .aspectRatio(0.7, contentMode: .fit)
+                }
+            }
+            // Set the card style
+            .pageViewStyle(.verticalCardDeck)
+            // Control how much of the card edges are visible
+            .scaleEffect(0.9)
+            // Card styling options
+            .pageViewCardCornerRadius(45.0)
+            .pageViewCardShadow(.visible)
+            // A tap gesture works great here
+            .onTapGesture {
+                print("Tapped card \(selection)")
+            }
+            
+            PageIndicator(
+                selection: indicatorSelection,
+                total: totalPages
+            )
+            .pageIndicatorColor(.secondary.opacity(0.3))
+            .pageIndicatorCurrentColor(selection.color)
+        }
+    }
+    
+    // Here's where you'd map your selection to a page index.
+    // In this example it's just the selection minus one.
+    var indicatorSelection: Binding<Int> {
+        .init {
+            selection - 1
+        } set: { newValue in
+            selection = newValue + 1
+        }
+    }
+}
+
+// MARK: - Preview
+
+@available(macOS, unavailable)
+struct VerticalCardDeckExample_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        VerticalCardDeckExample()
+            .environment(\.layoutDirection, .rightToLeft)
+    }
+}
---
+++ Sources/BigUIPaging/Implementations/PageView/Styles/VerticalCardDeckPageViewStyle.swift
@@ -0,0 +1,182 @@
+//  VerticalCardDeckPageViewStyle.swift
+//  BigUIPagingArabic
+//
+//  Created by iSlam AbdelAziz on 07/12/2025.
+//
+
+import SwiftUI
+
+/// A style that arranges pages as a whimsical deck of cards in a vertical stack.
+///
+/// This style mimics the behaviour of the photo stack in iMessage and Big News, but vertically.
+///
+@available(macOS, unavailable)
+@available(iOS 16.0, *)
+public struct VerticalCardDeckPageViewStyle: PageViewStyle {
+    
+    public init() { }
+    
+    public func makeBody(configuration: Configuration) -> some View {
+        VerticalCardDeckPageView(configuration)
+    }
+}
+
+struct VerticalCardDeckPageView: View {
+    
+    typealias Value = PageViewStyleConfiguration.Value
+    typealias Configuration = PageViewStyleConfiguration
+    
+    struct Page: Identifiable {
+        let index: Int
+        let value: Value
+        
+        var id: Value {
+            return value
+        }
+    }
+    
+    let configuration: Configuration
+    
+    @State private var dragProgress = 0.0
+    @State private var selectedIndex = 0
+    @State private var pages = [Page]()
+    @State private var containerSize = CGSize.zero
+    
+    @Environment(\.cardCornerRadius) private var cornerRadius
+    @Environment(\.cardShadowDisabled) private var shadowDisabled
+
+    init(_ configuration: Configuration) {
+        self.configuration = configuration
+    }
+    
+    var body: some View {
+        ZStack {
+            ForEach(pages) { page in
+                configuration.content(page.value)
+                    .cardStyle(cornerRadius: cornerRadius)
+                    .zIndex(zIndex(for: page.index))
+                    .offset(y: yOffset(for: page.index))
+                    .scaleEffect(scale(for: page.index))
+//                    .rotationEffect(.degrees(rotation(for: page.index)))
+                    .shadow(color: shadow(for: page.index), radius: 30, y: 20)
+            }
+        }
+        .measure($containerSize)
+        .scaleEffect(0.8)
+        .highPriorityGesture(dragGesture)
+        .task {
+            makePages(from: configuration.selection.wrappedValue)
+        }
+        .onChange(of: selectedIndex) { newValue in
+            configuration.selection.wrappedValue = pages[newValue].value
+        }
+        .onChange(of: configuration.selection.wrappedValue) { newValue in
+            makePages(from: newValue)
+            self.dragProgress = 0.0
+        }
+    }
+    
+    func makePages(from value: Value) {
+        let (values, index) = configuration.values(surrounding: value)
+        self.pages = values.enumerated().map {
+            Page(index: $0.offset, value: $0.element)
+        }
+        self.selectedIndex = index
+    }
+    
+    var dragGesture: some Gesture {
+        DragGesture(minimumDistance: 5)
+            .onChanged { value in
+                // Vertical drag
+                self.dragProgress = -(value.translation.height / containerSize.height)
+            }
+            .onEnded { value in
+                snapToNearestIndex()
+            }
+    }
+    
+    func snapToNearestIndex() {
+        let threshold = 0.3
+        if abs(dragProgress) < threshold {
+            withAnimation(.bouncy) {
+                self.dragProgress = 0.0
+            }
+        } else {
+            let direction = dragProgress < 0 ? 1 : -1 // Drag up (negative) -> next item (increment index), Drag down (positive) -> prev item
+            withAnimation(.smooth(duration: 0.25)) {
+                go(to: selectedIndex + direction)
+                self.dragProgress = 0.0
+            }
+        }
+    }
+    
+    func go(to index: Int) {
+        let maxIndex = pages.count - 1
+        if index > maxIndex {
+            self.selectedIndex = maxIndex
+        } else if index < 0 {
+            self.selectedIndex = 0
+        } else {
+            self.selectedIndex = index
+        }
+        self.dragProgress = 0
+    }
+    
+    func currentPosition(for index: Int) -> Double {
+        progressIndex - Double(index)
+    }
+    
+    // MARK: - Geometry
+    
+    var progressIndex: Double {
+        dragProgress + Double(selectedIndex)
+    }
+    
+    func zIndex(for index: Int) -> Double {
+        let position = currentPosition(for: index)
+        return -abs(position)
+    }
+    
+    func yOffset(for index: Int) -> Double {
+        let padding = containerSize.height / 10
+        let y = (Double(index) - progressIndex) * padding
+        let maxIndex = pages.count - 1
+        
+        if index == selectedIndex && progressIndex < Double(maxIndex) && progressIndex > 0 {
+            return y * swingOutMultiplier
+        }
+        return y
+    }
+    
+    var swingOutMultiplier: Double {
+        return abs(sin(Double.pi * progressIndex) * 20)
+    }
+    
+    func scale(for index: Int) -> CGFloat {
+        return 1.0 - (0.1 * abs(currentPosition(for: index)))
+    }
+    
+    func rotation(for index: Int) -> Double {
+        return -currentPosition(for: index) * 2
+    }
+    
+    func shadow(for index: Int) -> Color {
+        guard shadowDisabled == false else {
+            return .clear
+        }
+        let index = Double(index)
+        let progress = 1.0 - abs(progressIndex - index)
+        let opacity = 0.3 * progress
+        return .black.opacity(opacity)
+    }
+}
+
+@available(macOS, unavailable)
+@available(iOS 16.0, *)
+extension PageViewStyle where Self == VerticalCardDeckPageViewStyle {
+    
+    /// A style that presents pages as a whimsical deck of cards in a vertical stack.
+    public static var verticalCardDeck: VerticalCardDeckPageViewStyle {
+        VerticalCardDeckPageViewStyle()
+    }
+}
